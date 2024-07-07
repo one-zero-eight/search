1fb798c479c36081215def4b6114dc8c6dadbd38
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-07-07 12:18:25 +0300

    chore: move auth to modules

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,6 +4,6 @@ from typing import TypeAlias, Annotated
 
 from fastapi import Depends
 
-from src.api.auth.dependencies import verify_user
+from src.modules.auth.dependencies import verify_user
 
 VerifiedDep: TypeAlias = Annotated[str, Depends(verify_user)]
