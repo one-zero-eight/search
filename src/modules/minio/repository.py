78d7318dd0330b90b6227797cb760fe8e981bfa1
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-07-13 18:21:47 +0300

    chore: remove minio data

--- src/modules/minio/repository.py
+++ src/modules/minio/repository.py
@@ -3,7 +3,7 @@ from minio.error import S3Error
 
 from src.api.logging_ import logger
 from src.config import settings
-from src.modules.minio.schemas import MinioData, MoodleFileObject
+from src.modules.minio.schemas import MoodleFileObject
 from src.storages.minio import minio_client
 
 
@@ -26,20 +26,7 @@ class MinioRepository:
                         module_id = int(parts[2])
                         filename = parts[3]
 
-                        # Retrieve the object metadata
-                        object_stat = self.minio_client.stat_object(settings.minio.bucket, obj.object_name)
-
-                        moodle_object = MoodleFileObject(
-                            course_id=course_id,
-                            module_id=module_id,
-                            filename=filename,
-                            minio_data=MinioData(
-                                size=object_stat.size,
-                                last_modified=object_stat.last_modified,
-                                object_name=obj.object_name,
-                                metadata=dict(object_stat.metadata) if object_stat.metadata is not None else None,
-                            ),
-                        )
+                        moodle_object = MoodleFileObject(course_id=course_id, module_id=module_id, filename=filename)
                         moodle_objects.append(moodle_object)
                     except (ValueError, S3Error) as e:
                         logger.error(f"Error processing object {obj.object_name}: {e}")
--- src/modules/minio/schemas.py
+++ src/modules/minio/schemas.py
@@ -1,18 +1,7 @@
-import datetime
-
-
 from src.custom_pydantic import CustomModel
-
-
-class MinioData(CustomModel):
-    size: int | None
-    last_modified: datetime.datetime | None
-    object_name: str
-    metadata: dict[str, str] | None
 
 
 class MoodleFileObject(CustomModel):
     course_id: int
     module_id: int
     filename: str
-    minio_data: MinioData
