7846919b0332e6fa19557a6978739553fd616752
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-09-22 18:52:03 +0300

    fix: typo

--- src/api/app.py
+++ src/api/app.py
@@ -36,7 +36,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=settings.api_settings.cors_allow_origins_regex,
+    allow_origin_regex=settings.api_settings.cors_allow_origin_regex,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
