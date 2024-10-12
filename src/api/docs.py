378bdc6f7337ba20a2d97c4482f0763beac06cc2
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-10-12 22:43:49 +0300

    docs: add some info in Swagger docs

--- src/api/docs.py
+++ src/api/docs.py
@@ -6,11 +6,24 @@ from fastapi.routing import APIRoute
 VERSION = "0.1.0"
 
 # Info for OpenAPI specification
-TITLE = "InNoHassle-Search"
+TITLE = "InNoHassle Search API"
 SUMMARY = "Search service for InNoHassle ecosystem."
 
 DESCRIPTION = """
 ### About this project
+
+This is the API for Search project in InNoHassle ecosystem developed by one-zero-eight community.
+
+Using this API you can search among Moodle documents.
+
+Backend is developed using FastAPI framework on Python.
+
+Note: API is unstable. Endpoints and models may change in the future.
+
+Useful links:
+- [Search API source code](https://github.com/one-zero-eight/search)
+- [Search ML Experiments source code](https://github.com/one-zero-eight/search-experiments)
+- [InNoHassle Website](https://innohassle.ru/)
 """
 
 CONTACT_INFO = {
