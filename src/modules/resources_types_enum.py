9f86113e3dd36883a7b401d6224c8a5c23eb2db1
Vladimir Paskal <vpd63@inbox.ru>
2025-07-20 17:34:07 +0300

    feat: extend static resources

--- src/modules/resources_types_enum.py
+++ src/modules/resources_types_enum.py
@@ -5,3 +5,7 @@ class Resources(StrEnum):
     innohassle = "innohassle"
     myuni = "myuni"
     ithelp = "ithelp"
+    academic_calendar = "academic_calendar"
+    psychologist = "psychologist"
+    university_events = "university_events"
+    university_store = "university_store"
