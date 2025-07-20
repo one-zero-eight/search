b0d6afb135ecf40dd4090bb5ad1f8f8c2d80c2d1
AzaliaAlisheva <azalia.alischeva@yandex.ru>
2025-07-20 23:49:51 +0300

    feat: add metrics script

---
+++ scripts/search_result_metrics.py
@@ -0,0 +1,100 @@
+import asyncio
+
+import httpx
+import numpy as np
+import pandas as pd
+from irmetrics.topk import rr
+
+
+async def pred(i, question, expected_url, token):
+    async with httpx.AsyncClient(timeout=10) as client:
+        response = await client.get(
+            "http://127.0.0.1:8001/search/search",
+            params={
+                "query": question,
+                "response_types": ["link_to_source"],
+            },
+            headers={"Authorization": f"Bearer {token}"},
+        )
+
+    response_data = response.json()
+    if not response_data["responses"]:
+        return []
+
+    return [int(item.get("source", "").get("url", "") == expected_url) for item in response_data["responses"]]
+
+
+def count_ndcg(relevance_scores, k=None):
+    if not relevance_scores:
+        return 0
+    scores = np.asarray(relevance_scores)[:k]
+    discounts = np.log2(np.arange(2, len(scores) + 2))
+    dcg = np.sum(scores / discounts)
+    idcg = np.sum(discounts)
+    return dcg / idcg
+
+
+def count_average_precision(relevance_scores, k=None):
+    if not relevance_scores:
+        return 0
+    scores = np.asarray(relevance_scores)[:k]
+    rel_cumsum = np.cumsum(scores)
+
+    precision_at_k = rel_cumsum / (np.arange(len(scores)))
+
+    ap = np.sum(precision_at_k * scores) / np.sum(scores)
+
+    return ap
+
+
+async def get_preds(df, token):
+    result = []
+    for i, question in enumerate(df["Вопрос"]):
+        pred_list = await pred(i, question, df["Ссылка"][i], token)
+        result.append(pred_list)
+    return result
+
+
+def count_metrics(pred_lists):
+    response_rates = []
+    aps = []
+    ndcgs = []
+
+    for pred_list in pred_lists:
+        response_rate = rr(1, pred_list) if pred_list else 0
+        ap = sum(pred_list) / len(pred_list) if pred_list else 0
+        ndcg = count_ndcg(pred_list) if pred_list else 0
+
+        response_rates.append(response_rate)
+        aps.append(ap)
+        ndcgs.append(ndcg)
+
+    print(f"Mean Response Rate: {np.array(response_rates).mean()}")
+    print(f"Mean Average Precision: {np.array(aps).mean()}")
+    print(f"Mean NDCG: {np.array(ndcgs).mean()}")
+
+
+async def process():
+    SHEET_ID = "1kpxqZB_gEwnivJHe5jJCVE0o_zqLAYw_Q_xQO3SwplI"
+
+    en_url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid=0"
+    ru_url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid=53855511"
+    en_df = pd.read_csv(en_url)
+    ru_df = pd.read_csv(ru_url)
+
+    token = input("Enter your token from https://innohassle.ru/account/token: ")
+    en_preds = await get_preds(en_df, token)
+    ru_preds = await get_preds(ru_df, token)
+
+    print("Metrics for English queries")
+    count_metrics(en_preds)
+    print("---")
+    print("Metrics for Russian queries")
+    count_metrics(ru_preds)
+    print("---")
+    print("Metrics for both")
+    count_metrics(en_preds + ru_preds)
+
+
+if __name__ == "__main__":
+    asyncio.run(process())
